90d04b8903243ef3414138fae6f7edaf9af8e8b2
ismashaaaa <mashaderduik@gmail.com>
2024-07-23 17:07:45 +0300

    Added methods AddCustomValidators and AddValidationServiceWithAllValidators

--- MetalReleaseTracker/MetalReleaseTracker.API/Program.cs
+++ MetalReleaseTracker/MetalReleaseTracker.API/Program.cs
@@ -1,17 +1,12 @@
-﻿using FluentValidation;
-using MetalReleaseTracker.API.Middleware;
-using MetalReleaseTracker.Core.Entities;
-using MetalReleaseTracker.Core.Filters;
+﻿using MetalReleaseTracker.API.Middleware;
+using MetalReleaseTracker.API.ValidationConfiguration;
 using MetalReleaseTracker.Core.Interfaces;
 using MetalReleaseTracker.Core.Services;
-using MetalReleaseTracker.Core.Validators;
 using MetalReleaseTracker.Infrastructure.Data;
 using MetalReleaseTracker.Infrastructure.Data.MappingProfiles;
 using MetalReleaseTracker.Infrastructure.Repositories;
 using MetalReleaseTracker.Сore.Services;
-
 using Microsoft.EntityFrameworkCore;
-
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -41,17 +36,8 @@ builder.Services.AddScoped<IBandService, BandService>();
 builder.Services.AddScoped<IDistributorsService, DistributorsService>();
 builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
 
-builder.Services.AddTransient<IValidator<Album>, AlbumValidator>();
-builder.Services.AddTransient<IValidator<AlbumFilter>, AlbumFilterValidator>();
-builder.Services.AddTransient<IValidator<Band>, BandValidator>();
-builder.Services.AddTransient<IValidator<Distributor>, DistributorValidator>();
-builder.Services.AddTransient<IValidator<Subscription>, SubscriptionValidator>();
-builder.Services.AddTransient<IValidator<Guid>, GuidValidator>();
-builder.Services.AddTransient<IValidationService>(serviceProvider =>
-{
-    var validators = FluentValidationConfigurationExtensions.GetFluentValidators(serviceProvider);
-    return new ValidationService(validators);
-});
+builder.Services.AddCustomValidators();
+builder.Services.AddValidationServiceWithAllValidators();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
---
+++ MetalReleaseTracker/MetalReleaseTracker.API/ValidationConfiguration/FluentValidationConfigurationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using MetalReleaseTracker.Core.Entities;
+using MetalReleaseTracker.Core.Filters;
+using MetalReleaseTracker.Core.Interfaces;
+using MetalReleaseTracker.Core.Validators;
+
+namespace MetalReleaseTracker.API.ValidationConfiguration
+{
+    public static class FluentValidationConfigurationExtensions
+    {
+        public static IServiceCollection AddCustomValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<Album>, AlbumValidator>();
+
+            services.AddTransient<IValidator<AlbumFilter>, AlbumFilterValidator>();
+
+            services.AddTransient<IValidator<Band>, BandValidator>();
+
+            services.AddTransient<IValidator<Distributor>, DistributorValidator>();
+
+            services.AddTransient<IValidator<Subscription>, SubscriptionValidator>();
+
+            services.AddTransient<IValidator<Guid>, GuidValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddValidationServiceWithAllValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidationService, ValidationService>(serviceProvider =>
+            {
+                var validators = new List<IValidator>
+                {
+                    serviceProvider.GetRequiredService<IValidator<Album>>(),
+
+                    serviceProvider.GetRequiredService<IValidator<AlbumFilter>>(),
+
+                    serviceProvider.GetRequiredService<IValidator<Band>>(),
+
+                    serviceProvider.GetRequiredService<IValidator<Distributor>>(),
+
+                    serviceProvider.GetRequiredService<IValidator<Subscription>>(),
+
+                    serviceProvider.GetRequiredService<IValidator<Guid>>()
+                };
+
+                return new ValidationService(validators);
+            });
+
+            return services;
+        }
+    }
+}
