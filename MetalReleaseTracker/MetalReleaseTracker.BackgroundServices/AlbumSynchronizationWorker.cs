7d74fad3c59cf542a28b4b2759ce1f7d75dbe2b3
ismashaaaa <mashaderduik@gmail.com>
2024-10-15 20:09:13 +0300

    Keep working with Background Service

--- MetalReleaseTracker/MetalReleaseTracker.BackgroundServices/AlbumSynchronizationWorker.cs
+++ MetalReleaseTracker/MetalReleaseTracker.BackgroundServices/AlbumSynchronizationWorker.cs
@@ -8,23 +8,26 @@ namespace MetalReleaseTracker.BackgroundServices
         private readonly ILogger<AlbumSynchronizationWorker> _logger;
         private readonly IAlbumSynchronizationService _albumSynchronizationService;
         private readonly IRecurringJobManager _recurringJobManager;
+        private readonly string _cronExpression;
 
         public AlbumSynchronizationWorker(
              ILogger<AlbumSynchronizationWorker> logger,
              IAlbumSynchronizationService albumSynchronizationService,
-             IRecurringJobManager recurringJobManager
+             IRecurringJobManager recurringJobManager,
+             IConfiguration configuration
             )
         {
             _logger = logger;
             _albumSynchronizationService = albumSynchronizationService;
             _recurringJobManager = recurringJobManager;
+            _cronExpression = configuration["AlbumSynchronizationSettings:CronExpression"];
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("AlbumSynchronizationWorker is starting.");
 
-            _recurringJobManager.AddOrUpdate("SynchronizeAllAlbumsJob", () => _albumSynchronizationService.SynchronizeAllAlbums(), Cron.Daily);                               
+            _recurringJobManager.AddOrUpdate("SynchronizeAllAlbumsJob", () => _albumSynchronizationService.SynchronizeAllAlbums(), _cronExpression);                     
 
             _logger.LogInformation("SynchronizeAllAlbums task scheduled to run daily.");
             return base.StartAsync(cancellationToken);
@@ -35,10 +38,10 @@ namespace MetalReleaseTracker.BackgroundServices
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation($"The background service is working: {DateTimeOffset.Now}");
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
             }
         }
-
+        
         public override Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("AlbumSynchronizationWorker stopped.");
--- MetalReleaseTracker/MetalReleaseTracker.BackgroundServices/Program.cs
+++ MetalReleaseTracker/MetalReleaseTracker.BackgroundServices/Program.cs
@@ -1,18 +1,52 @@
 using Hangfire;
+using Hangfire.PostgreSql;
 using MetalReleaseTracker.Application.Interfaces;
 using MetalReleaseTracker.Application.Services;
 using MetalReleaseTracker.BackgroundServices;
+using MetalReleaseTracker.Core.Interfaces;
+using MetalReleaseTracker.Core.Services;
+using MetalReleaseTracker.Core.Validators;
+using MetalReleaseTracker.Infrastructure.Data;
+using MetalReleaseTracker.Infrastructure.Data.MappingProfiles;
+using MetalReleaseTracker.Infrastructure.Factories;
+using MetalReleaseTracker.Infrastructure.Repositories;
+using MetalReleaseTracker.Ñore.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Configuration.AddJsonFile("settings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddDbContext<MetalReleaseTrackerDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddHangfire(configuration => configuration
-    .UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    .UsePostgreSqlStorage(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 builder.Services.AddHangfireServer();
 
+builder.Services.AddScoped<IAlbumRepository, AlbumRepository>();
+builder.Services.AddScoped<IBandRepository, BandRepository>();
+builder.Services.AddScoped<IDistributorsRepository, DistributorsRepository>();
+
+builder.Services.AddScoped<IParserFactory, ParserFactory>();
+builder.Services.AddScoped<IValidationService, ValidationService>();
+builder.Services.AddScoped<IAlbumService, AlbumService>();
+builder.Services.AddScoped<IBandService, BandService>();
+builder.Services.AddScoped<IDistributorsService, DistributorsService>();
 builder.Services.AddScoped<IAlbumSynchronizationService, AlbumSynchronizationService>();
+
 builder.Services.AddHostedService<AlbumSynchronizationWorker>();
 
-var host = builder.Build();
-host.Run();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+var app = builder.Build();
+app.UseHangfireDashboard();
+app.Run();
