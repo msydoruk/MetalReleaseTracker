e6bd53c0c648a1915317e2754794b5700c473cbe
Misha Sydoruk <msydoruk@radiantrfid.com>
2024-10-26 17:55:36 +0300

    updated docker-compose.yml

--- MetalReleaseTracker/start-docker-compose.bat
+++ MetalReleaseTracker/start-docker-compose.bat
@@ -1,4 +1,6 @@
 cd c:\Projects\GitHubSolutions\MetalReleaseTracker\MetalReleaseTracker\
 docker-compose down
+docker rmi metalreleasetrackerbackgroundservices
+docker rmi metalreleasetrackerapi
 docker-compose -f docker-compose.yml up
 pause
